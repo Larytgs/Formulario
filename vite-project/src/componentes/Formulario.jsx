11cb5c3c008c8849c08bf61a8ef1f80a66525cad
laryTgs <laryssatortato99@gmail.com>
2025-06-06 17:49:29 -0300

    Update Formulario.jsx

--- vite-project/src/componentes/Formulario.jsx
+++ vite-project/src/componentes/Formulario.jsx
@@ -1,28 +1,55 @@
 function Formulario() {
   return (
-    <section className="flex">
-      <h1 className="text-[3em] ">Titulo</h1>
-      <div>
-        <p className="w-[300px] m-[10%]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-          illo fugit iusto commodi, soluta veniam eligendi ducimus delectus,
-          dolor enim animi unde vero ut, neque id repellendus error? Soluta,
-          provident!
-        </p>
-      </div>
+    <section>
+      <div className="flex mt-20 gap-10 justify-evenly">
+        <div className="text-center">
+          <h1 className="text-[1em] w-[250px] text-wrap mt-32 mb-5">
+            FICOU COM DÚVIDA? NOS MANDE UMA MENSAGEM!
+          </h1>
+          <p className="w-[250px]">
+            Caso tenha ficado com qualquer dúvida basta nos mandar uma mensagem
+            e entraremos em contato em menos de 24h.
+          </p>
+        </div>
 
-      <div className="flex">
-        <div>
-          <h1 className="text-[2em]">Formulário</h1>
-          <input type="text" value="name" placeholder="Nome completo: *" />
-          <input type="email" value="email" placeholder="Email: *" />
-          <input type="number" valur="number" placeholder="Telefone: *" />
-          <input type="text" value="test" placeholder="Mensagem: *" />
-        </div>
-        <div>
-          <p>* campos obrigatórios</p>
-        </div>
-        <input type="button" value="Enviar" />
+        <form className="border border-1 border-gray-800 bg-orange-50 rounded-xl p-5 mt-10">
+          <div className="flex flex-col gap-2">
+            <input
+              className="p-2 rounded-sm"
+              type="text"
+              placeholder="Nome completo: *"
+              required // para campo obrigatorio
+            />
+            <input
+              className="p-2 rounded-sm"
+              type="email"
+              placeholder="Email: *"
+              required
+            />
+            <input
+              className="p-2 rounded-sm"
+              type="tel"
+              placeholder="Telefone: *"
+              required
+            />
+            <textarea
+              className="p-2 rounded-sm"
+              name="msg"
+              cols="30"
+              rows="5"
+              placeholder="Mensagem: *"
+            ></textarea>
+          </div>
+
+          <div>
+            <p className="my-4">* campos obrigatórios</p>
+          </div>
+          <input
+            className="cursor-pointer px-5 py-1 mt-3 rounded-xl bg-green-500"
+            type="button"
+            value="Enviar"
+          />
+        </form>
       </div>
     </section>
   );
